ea36ce9b1ed56e1e29fcbf2dc12cc5eeae5b668b
Boris <Borismulder@outlook.com>
2023-01-29 21:11:00 +0100

    fixed wrong output name when compiling

--- CompileGame.ps1
+++ CompileGame.ps1
@@ -1,1 +1,1 @@
-gcc main.c -o main.exe -O1 -Wall -std=c99 -Wno-miising-braces -I includes/ -L libs/ -lraylib -opengl32 -lgdi32 -lwinmm
+gcc main.c -o main.exe -O1 -Wall -std=c99 -Wno-miising-braces -I includes/ -L libs/ -lraylib -lopengl32 -lgdi32 -lwinmm
